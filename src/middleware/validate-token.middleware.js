f3c35f09ed744bc2f5fa6b9f8cf3bd25c46c602c
IvanOliverFunesDev <ioliverfunes@gmail.com>
2025-04-09 14:06:29 +0200

    feat: Bloque auth completado (middlewares,controllers,routes)

---
+++ src/middleware/validate-token.middleware.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import config from '../config.js';
+
+const JWT_SECRET = config.security.JWT_SECRET;
+
+export const authRequired = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+
+  if (!token) return res.status(401).json({ message: 'Authentication token is missing' });
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(403).json({ message: 'Authentication token is invalid or expired' });
+    req.user = decoded;
+    next();
+  });
+};
+
+// export const authOptional = (req, res, next) => {
+//   const token = req.headers['authorization']?.split(' ')[1];
+//   if (!token) {
+//     req.user = undefined;
+//     return next();
+//   }
+
+//   jwt.verify(token, JWT_SECRET, (err, decoded) => {
+//     if (err) {
+//       req.user = undefined;
+//       return next();
+//     }
+//     req.user = decoded;
+//     next();
+//   });
+// };
