1113341b0cc90730cbb4d17c8df54fcf18339348
IvanOliverFunesDev <ioliverfunes@gmail.com>
2025-04-13 20:10:51 +0200

    feat: Eliminar(delte) rutina (controller y route)

--- src/controllers/routine.controller.js
+++ src/controllers/routine.controller.js
@@ -1,18 +1,18 @@
 import Routine from '../models/Routine.js';
 import { errorResponse, successResponse } from '../../utils/response.js';
+import { findPair } from 'yaml/util';
+import { ro } from '@faker-js/faker';
 
 export const createRoutineController = async (req, res) => {
   const userId = req.user.id;
   const { daysPerWeek, days } = req.body;
 
   try {
-    // Verificar si el usuario ya tiene una rutina creada
     const existingRoutine = await Routine.findOne({ user: userId });
     if (existingRoutine) {
       return errorResponse(res, 400, 'Ya tienes una rutina creada. No puedes crear más de una.');
     }
 
-    // Crear nueva rutina
     const newRoutine = new Routine({
       user: userId,
       daysPerWeek,
@@ -39,5 +39,30 @@ export const getRoutineController = async (req, res) => {
 
   } catch (error) {
     return errorResponse(res, 500, 'Error al obtener la rutina', [{ message: error.message }]);
+  }
+}
+
+export const deleteRoutineController = async (req, res) => {
+  const userId = req.user.id;
+  const routineId = req.params.id;
+  console.log(routineId);
+
+  try {
+    const routine = await Routine.findById(routineId);
+
+    if (!routine) {
+      return errorResponse(res, 404, 'La rutina no existe');
+    }
+
+    if (routine.user.toString() !== userId) {
+      return errorResponse(res, 403, 'No tienes permiso para eliminar esta rutina');
+    }
+
+    await Routine.findOneAndDelete({ _id: routineId });
+
+    return successResponse(res, 'Rutina eliminada correctamente');
+
+  } catch (error) {
+    return errorResponse(res, 500, 'Error al eliminar la rutina', [{ message: error.message }]);
   }
 }
--- src/routes/routines.routes.js
+++ src/routes/routines.routes.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import { authRequired } from '../middleware/verify-token.middleware.js';
 import { validateSchema } from '../middleware/validator-schema.middleware.js';
 import { createRoutineSchema } from '../schemas/create-routine.schema.js';
-import { createRoutineController, getRoutineController } from '../controllers/routine.controller.js';
+import { createRoutineController, getRoutineController, deleteRoutineController } from '../controllers/routine.controller.js';
 
 const router = Router();
 
-router.post('/routine', authRequired, validateSchema(createRoutineSchema), createRoutineController);
-router.get('/routine', authRequired, getRoutineController);
+router.post('/', authRequired, validateSchema(createRoutineSchema), createRoutineController);
+router.get('/', authRequired, getRoutineController);
+router.delete('/:id', authRequired, deleteRoutineController);
 
 export default router;
