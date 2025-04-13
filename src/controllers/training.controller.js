5a38aa638ee6a3a5de8c60895aed7973d642eac0
IvanOliverFunesDev <ioliverfunes@gmail.com>
2025-04-14 00:47:13 +0200

    feat: Crear Comparacion entre primer y ultimo entreno de día seleccionado (controller and route)

--- src/controllers/training.controller.js
+++ src/controllers/training.controller.js
@@ -49,3 +49,55 @@ export const getTrainingsByDayController = async (req, res) => {
     return errorResponse(res, 500, 'Error al obtener entrenamientos por día', [{ message: error.message }]);
   }
 }
+
+export const compareTrainingsController = async (req, res) => {
+  const userId = req.user.id;
+  const dayNumber = parseInt(req.params.routineDayNumber);
+
+  try {
+    const trainings = await Training.find({
+      user: userId,
+      routineDayNumber: dayNumber
+    }).sort({ createdAt: 1 });
+
+    if (trainings.length < 2) {
+      return errorResponse(res, 400, 'Necesitas al menos dos entrenamientos para comparar');
+    }
+
+    const first = trainings[0];
+    const last = trainings[trainings.length - 1];
+
+    const result = [];
+
+    for (let i = 0; i < first.exercises.length; i++) {
+      const exFirst = first.exercises[i];
+      const exLast = last.exercises.find(e => e.name === exFirst.name);
+
+      if (!exLast) continue;
+
+      const comparison = {
+        name: exFirst.name,
+        seriesComparison: []
+      };
+
+      for (let j = 0; j < exFirst.series.length; j++) {
+        const s1 = exFirst.series[j];
+        const s2 = exLast.series[j];
+
+        if (!s2) continue;
+
+        comparison.seriesComparison.push({
+          serie: j + 1,
+          weightDiff: s2.weight - s1.weight,
+          repsDiff: s2.reps - s1.reps
+        });
+      }
+
+      result.push(comparison);
+    }
+
+    return successResponse(res, `Comparación del Día ${dayNumber}`, result);
+  } catch (error) {
+    return errorResponse(res, 500, 'Error al comparar entrenamientos', [{ message: error.message }]);
+  }
+};
--- src/routes/training.routes.js
+++ src/routes/training.routes.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import { authRequired } from '../middleware/verify-token.middleware.js';
 import { validateSchema } from '../middleware/validator-schema.middleware.js';
 import { createTrainingSchema } from '../schemas/training/create-training.js'
-import { createTrainingController, getTrainingsByDayController, getUserTrainingsController } from '../controllers/training.controller.js';
+import { compareTrainingsController, createTrainingController, getTrainingsByDayController, getUserTrainingsController } from '../controllers/training.controller.js';
 
 const router = Router();
 
 router.post('/', authRequired, validateSchema(createTrainingSchema), createTrainingController);
 router.get('/user', authRequired, getUserTrainingsController);
 router.get('/user/day/:routineDayNumber', authRequired, getTrainingsByDayController);
+router.get('/compare/:routineDayNumber', authRequired, compareTrainingsController);
 
 export default router;
